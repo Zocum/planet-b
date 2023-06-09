3357d75d31cb06cd7ec1fc766b6d109141864e7d
hjimenezcruz <hector.jimenezcruz@kittelberger.de>
2023-06-09 23:26:39 +0200

    buttons configured, page slice removed

--- app/page.tsx
+++ app/page.tsx
@@ -6,14 +6,19 @@ import styles from './page.module.scss';
 interface ImageData {
   img_src: string;
 }
-
 interface RoverManifest {
   photo_manifest: {
-    max_sol: number;
+    max_sol: number,
+    photos: {
+      sol: number;
+      total_photos: number;
+      cameras: string[];
+    }[]
   }
 }
 
-async function fetchRoverManifest(): Promise<RoverManifest> {
+
+async function fetchRoverManifest(): Promise<RoverManifest['photo_manifest']['photos']> {
   const API_KEY = 'vTvXej37nbIqMhzi3GwjmXSi1IJpfLqeKl6r7zWa'; // Replace with your API key
   const response = await fetch(`https://api.nasa.gov/mars-photos/api/v1/manifests/curiosity?api_key=${API_KEY}`);
 
@@ -22,7 +27,7 @@ async function fetchRoverManifest(): Promise<RoverManifest> {
   }
 
   const data = await response.json();
-  return data;
+  return data.photo_manifest.photos;
 }
 
 async function fetchImages(sol: number, page: number): Promise<ImageData[]> {
@@ -41,49 +46,90 @@ export default function Home() {
   const [images, setImages] = useState<ImageData[]>([]);
   const [currentSol, setCurrentSol] = useState<number | null>(null);
   const [currentPage, setCurrentPage] = useState(1);
+  const [maxSol, setMaxSol] = useState<number | null>(null);
+  const [manifestPhotos, setManifestPhotos] = useState<RoverManifest['photo_manifest']['photos']>([]);
+  const [displayedImages, setDisplayedImages] = useState(0);
 
   useEffect(() => {
     fetchRoverManifest()
-    .then(manifest => setCurrentSol(manifest.photo_manifest.max_sol))
-    .catch(console.error);
-  }, [])
+      .then(photos => {
+        setManifestPhotos(photos);
+        const maxSol = photos[photos.length - 1].sol;
+        setCurrentSol(maxSol);
+        setMaxSol(maxSol);
+      })
+      .catch(console.error);
+  }, [setMaxSol]);
 
   useEffect(() => {
     if (currentSol !== null) {
-      fetchImages(currentSol, currentPage).then(setImages).catch(console.error);
+      fetchImages(currentSol, 1)
+        .then((fetchedImages) => {
+          const imagesToDisplay = fetchedImages.slice(0, 16);
+          setImages(fetchedImages);
+          setDisplayedImages(imagesToDisplay.length);
+        })
+        .catch(console.error);
     }
   }, [currentSol, currentPage]);
-   console.log(images);
 
+  console.log(images);
+
+  const totalPhotosForCurrentSol = manifestPhotos.find(photo => photo.sol === currentSol)?.total_photos || 0;
+  
+  // Handle next and previous sol while keeping track of how many images are shown
+  const handleNextPage = () => {
+    setCurrentPage((old: number) => {
+      const newPage = old + 1;
+      const imagesOnNewPage = images.slice((newPage - 1) * 16, newPage * 16);
+      setDisplayedImages(imagesOnNewPage.length);
+      return newPage;
+    });
+  };
+
+  const handlePrevPage = () => {
+    setCurrentPage((old: number) => {
+      const newPage = Math.max(old - 1, 1);
+      const imagesOnNewPage = images.slice((newPage - 1) * 16, newPage * 16);
+      setDisplayedImages(imagesOnNewPage.length);
+      return newPage;
+    });
+  };
+  
   return (
     <main className={styles.main}>
       <h1 className={styles.title}>Mars Curiosity Images</h1>
-      <h2 className={styles.subtitle}>Currently viewing <p className={styles.subtitle_sol}>Sol {currentSol}</p></h2>
+      <h2 className={styles.subtitle}>Currently viewing:</h2>
+      <div className={styles.solButtons}>
+        <button className={styles.solButtons_prev} onClick={() => setCurrentSol((old: number | null) => Math.max((old || 1) - 1, 1))}>
+          Previous
+        </button>
+        <h1 className={styles.subtitle_sol}>Sol {currentSol}</h1>
+        <button className={currentSol === maxSol ? styles.solButtons_next_disabled : styles.solButtons_next} 
+         onClick={() => setCurrentSol((old: number | null) => (old || 1) + 1)}
+         disabled={currentSol === maxSol}>
+          Next
+        </button>
+      </div>
+
+      <div className={styles.pageButtons}>
+        <button className={styles.pageButtons_prev} onClick={handlePrevPage}>
+          Previous Page
+        </button>
+        <h3 className={styles.photoNumber}>{images.length} items of {totalPhotosForCurrentSol}</h3>
+        <button className={styles.pageButtons_next} onClick={handleNextPage}>
+          Next Page
+        </button>
+      </div>
 
       <div className={styles.grid}>
-        {images.slice(0, 16).map((image, index) => (
+        {images.map((image, index) => (
           <div key={index} className={styles.card}>
             <Image className={styles.image} src={image.img_src} alt="NASA Mars Rover Image" width={180} height={180} layout='responsive' />
           </div>
         ))}
       </div>
 
-      <div className={styles.solButtons}>
-        <button onClick={() => setCurrentSol((old: number | null) => Math.max(old || 1 - 1, 1))}>
-          Previous Sol
-        </button>
-        <button onClick={() => setCurrentSol((old: number | null) => (old || 1) + 1)}>
-          Next Sol
-        </button>
-      </div>
-      <div className={styles.pageButtons}>
-        <button onClick={() => setCurrentPage((old: number) => Math.max(old - 1, 1))}>
-          Previous Page
-        </button>
-        <button onClick={() => setCurrentPage((old: number) => old + 1)}>
-          Next Page
-        </button>
-      </div>
     </main>
   );
 }
