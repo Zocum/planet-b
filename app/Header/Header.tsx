6e932203ea25cd1538c1e191dbbc07e9939ff140
hjimenezcruz <hector.jimenezcruz@kittelberger.de>
2023-08-14 14:48:46 +0200

    cleanup header  + footer files

--- app/Header/Header.tsx
+++ app/Header/Header.tsx
@@ -1,12 +1,10 @@
-import { useEffect, useRef, useState } from 'react';
+import { useState } from 'react';
 import './Header.scss';
 import { HeaderProps } from '@/types';
 import Accordion from '../SummaryAccordion';
 
 export default function Header({ setRover }: HeaderProps) {
-    // const detailsRef = useRef<HTMLDetailsElement | null>(null);
     const [isDetailsOpen, setIsDetailsOpen] = useState<boolean>(true);
-    const [isOpen, setIsOpen] = useState<boolean>(false);
 
     return (
         <header className='header'>
@@ -14,10 +12,8 @@ export default function Header({ setRover }: HeaderProps) {
                 <div className='header-nav-container'>
                     <div className='header-nav-details_wrapper'>
                         <Accordion>
-                            <summary aria-label='List of Mars Rovers' aria-hidden={isDetailsOpen} onClick={() => {
-                                setIsDetailsOpen(!isDetailsOpen);
-                                setIsOpen(prev => !prev);
-                                }}>Mars Rovers<div className={`${isOpen ? '-open' : ''}`}>{'>'}</div></summary>
+                            <summary aria-label='List of Mars Rovers' aria-hidden={isDetailsOpen} onClick={() => 
+                                setIsDetailsOpen(!isDetailsOpen)}>Mars Rovers<div>{'>'}</div></summary>
                             <div className="button_wrapper content">
                                 <button onClick={() => setRover('perseverance')}>Perseverance</button>
                                 <button onClick={() => setRover('curiosity')}>Curiosity</button>
