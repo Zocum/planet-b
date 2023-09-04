4201fca34c475c117c4a4abf3a5e407904282143
Zocum <hjclp18@gmail.com>
2023-09-04 22:48:12 +0200

    indentations

--- app/Footer/Footer.tsx
+++ app/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import Link  from 'next/link';
 export default function Footer() {
   return (
     <footer className={styles.footerWrapper}>
-      <p>© 2021 - Made by <a href='' target='_blank'>Hector Jimenez Cruz</a></p>
+      <p>© 2023 - Made by <a href='' target='_blank'>Hector Jimenez Cruz</a></p>
       <div className={styles.footerWrapper_contact}>
         <Link href='../contact'>About</Link>
       </div>
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -16,17 +16,18 @@ export default function Contact() {
 
   return (
     <>
-    <header style={headerStyles}> 
-    <h1>About</h1>
-    <h2>
-      <Link href="/">Back Home</Link>
-    </h2></header>
-   
-    <h4 style={styles}>Welcome to our Martian Gallery, where we bring the fascinating visuals of the Martian surface directly to your screen. Our portal serves as a gateway to experience the Martian environment through the lenses of NASA&lsquo;s Mars rovers.</h4>
-    <br />
-    <h3 style={styles}>Source of Our Images</h3>
-    <br />
-    <h4 style={styles}>The images exhibited on our gallery are sourced directly from NASA's Mars Rover Photos API. We value transparency and wish to acknowledge NASA&lsquo;s significant contribution to the realization of this project.</h4>
+      <header style={headerStyles}> 
+        <h1>About</h1>
+        <h2>
+          <Link href="/">Back Home</Link>
+        </h2>
+      </header>
+    
+      <h4 style={styles}>Welcome to our Martian Gallery, where we bring the fascinating visuals of the Martian surface directly to your screen. Our portal serves as a gateway to experience the Martian environment through the lenses of NASA&lsquo;s Mars rovers.</h4>
+      <br />
+      <h3 style={styles}>Source of Our Images</h3>
+      <br />
+      <h4 style={styles}>The images exhibited on our gallery are sourced directly from NASA's Mars Rover Photos API. We value transparency and wish to acknowledge NASA&lsquo;s significant contribution to the realization of this project.</h4>
     </>
   );
 };
